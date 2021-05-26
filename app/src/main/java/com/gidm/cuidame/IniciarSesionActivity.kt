9ebb9e0b71a315c3ef88a3ac0c232f8660f62d1b
mjls130598 <mjls130598@gmail.com>
2021-05-26 11:22:14 +0200

    Crea la visualización de registrar usuario Close #7

--- app/src/main/java/com/gidm/cuidame/IniciarSesionActivity.kt
+++ app/src/main/java/com/gidm/cuidame/IniciarSesionActivity.kt
@@ -3,9 +3,9 @@ package com.gidm.cuidame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class IniciarSesionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_iniciar_sesion)
     }
 }
