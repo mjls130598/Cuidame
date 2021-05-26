2fb6dac2d5b732b0189f30de9b3e3a508d735e4a
mjls130598 <mjls130598@gmail.com>
2021-05-26 13:30:14 +0200

    Crea acciones de registrar usuario #9

--- app/src/main/java/com/gidm/cuidame/IniciarSesionActivity.kt
+++ app/src/main/java/com/gidm/cuidame/IniciarSesionActivity.kt
@@ -1,11 +1,23 @@
 package com.gidm.cuidame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class IniciarSesionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_iniciar_sesion)
+
+        val registrarse = findViewById<Button>(R.id.registrarse)
+
+        // Si le da al botón "Registrarse", ...
+        registrarse.setOnClickListener{
+
+            // Se va a la Activity RegistrarseActivity
+            val intent = Intent(this, RegistrarseActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/gidm/cuidame/RegistrarseActivity.kt
+++ app/src/main/java/com/gidm/cuidame/RegistrarseActivity.kt
@@ -2,10 +2,33 @@ package com.gidm.cuidame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class RegistrarseActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_datos_usuario)
+
+        // Obtenemos los elementos de la vista
+        val nombreInput = findViewById<EditText>(R.id.inputNombre)
+        val correoInput = findViewById<EditText>(R.id.inputNuevoEmail)
+        val contraseniaInput = findViewById<EditText>(R.id.inputNewContrasenia)
+        val contraseniaRepInput = findViewById<EditText>(R.id.inputNewContrasenia2)
+        val guardar = findViewById<Button>(R.id.nuevoUsuario)
+
+        // Si se pulsa sobre "Guardar", ...
+        guardar.setOnClickListener{
+
+            val nombre = nombreInput.text.toString()
+            val correo = correoInput.text.toString()
+            val contrasenia = contraseniaInput.text.toString()
+            val contraseniaRep =  contraseniaRepInput.text.toString()
+
+            // Si los datos introducidos son correctos, ...
+            if(Utils.comprobarUsuario(nombre, correo, contrasenia, contraseniaRep, this)){
+            }
+        }
     }
 }
