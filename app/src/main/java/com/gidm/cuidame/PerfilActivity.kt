c6de41fad0a7482d3b77722d208ee7b0b774411f
mjls130598 <mjls130598@gmail.com>
2021-05-27 10:03:34 +0200

    Realiza el borrado de la cuenta Close #21 Close #19

--- app/src/main/java/com/gidm/cuidame/PerfilActivity.kt
+++ app/src/main/java/com/gidm/cuidame/PerfilActivity.kt
@@ -1,10 +1,15 @@
 package com.gidm.cuidame
 
+import android.app.Dialog
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -13,6 +18,13 @@ import com.google.firebase.database.ValueEventListener
 
 
 class PerfilActivity : AppCompatActivity() {
+
+    lateinit var dialogFragment: AlertaDialogFragment
+    private val usuario = FirebaseAuth.getInstance().currentUser!!
+    private val datosUsuario = FirebaseDatabase.getInstance().
+    getReference("Usuarios").child(usuario.uid)
+    private lateinit var shared : SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
@@ -23,23 +35,18 @@ class PerfilActivity : AppCompatActivity() {
         val editar = findViewById<Button>(R.id.editar)
         val borrar = findViewById<Button>(R.id.borrar)
 
+        shared = getSharedPreferences("datos-paciente", MODE_PRIVATE)
+
         // Obtenemos el id del usuario
-        val shared = getSharedPreferences("datos-paciente", MODE_PRIVATE)
         val id = shared.getString("id", null)
 
-        // Creamos instancia con la base de datos encargada de las autorizaciones
-        val auth = FirebaseAuth.getInstance()
-
         if (id != null){
-            val usuario = auth.currentUser
-
             // Accedemos a los datos del usuario
-            val datosUsuario = FirebaseDatabase.getInstance().
-            getReference("Usuarios").child(id).child("nombre")
+            val nombreUsuario = datosUsuario.child("nombre")
 
             // Los mostramos por pantalla
             if(usuario != null){
-                datosUsuario.addListenerForSingleValueEvent(object : ValueEventListener {
+                nombreUsuario.addListenerForSingleValueEvent(object : ValueEventListener {
                     override fun onDataChange(dataSnapshot: DataSnapshot) {
                         nombre.text = dataSnapshot.getValue(String::class.java)
                     }
@@ -54,6 +61,64 @@ class PerfilActivity : AppCompatActivity() {
         editar.setOnClickListener{
             val intent = Intent(this, EditarActivity::class.java)
             startActivity(intent)
+        }
+
+        // Si se clickea sobre "Borrar cuenta", ...
+        borrar.setOnClickListener{
+            // Mostrar el diálogo de confirmación
+            dialogFragment = AlertaDialogFragment.newInstance()
+            dialogFragment!!.show(supportFragmentManager, "Alerta")
+        }
+    }
+
+    fun continuarCierre(continuar: Boolean) {
+        if (continuar) {
+
+            // Se borra la infomación del usuario en la BD
+            datosUsuario.removeValue()
+
+            // Se borra la autenticación
+            usuario.delete().addOnCompleteListener{
+                // Si se ha borrado correctamente
+                if(it.isSuccessful){
+                    // Borramos el id de la memoria local
+                    with(shared.edit()){
+                        remove("id")
+                        commit()
+                    }
+
+                    // Nos dirigimos a la actividad de iniciar sesión
+                    val intent = Intent(this, IniciarSesionActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                    finish()
+                }
+
+                else
+                    Toast.makeText(this, "No se puede borrar, inténtelo más tarde", Toast.LENGTH_LONG).show()
+
+            }
+        }
+        else dialogFragment!!.dismiss()
+    }
+
+    class AlertaDialogFragment : DialogFragment() {
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            return AlertDialog.Builder(requireActivity())
+                .setMessage("¿Está seguro?")
+                .setCancelable(false)
+                .setNegativeButton("No") { _, _ ->
+                    (activity as PerfilActivity).continuarCierre(false)
+                }
+                .setPositiveButton("Sí") { _, _ ->
+                    (activity as PerfilActivity).continuarCierre(true)
+                }.create()
+        }
+
+        companion object {
+            fun newInstance(): AlertaDialogFragment {
+                return AlertaDialogFragment()
+            }
         }
     }
 }
